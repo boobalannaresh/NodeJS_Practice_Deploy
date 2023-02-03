f7503962f6b83dc848b4db6200ce1942b0380039
boobalannaresh <bestforevermvi@gmail.com>
2023-02-03 08:53:41 +0530

    Auto Assign Port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const PORT = 4000;
+const PORT = process.env.PORT;  //// Auto Assign Port
 
 //// Mongo Connection
 const MONGO_URL = process.env.MONGO_URL  
