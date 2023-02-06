09d2036495f73ecd71fa480a34663a9fb63645f9
boobalannaresh <bestforevermvi@gmail.com>
2023-02-06 13:45:07 +0530

    convert

--- services/movies.service.js
+++ services/movies.service.js
@@ -1,21 +1,21 @@
 
 import { client } from "../index.js";
-import { ObjectId } from "mongodb";
+import {ObjectId} from "mongodb";
 
 
 
 export async function updateMovieByID(id, updateRequest) {
-  return await client.db("Ragav").collection("movies").updateOne({ _id: ObjectId(id)}, { $set: updateRequest });
+  return await client.db("Ragav").collection("movies").updateOne({ _id: new ObjectId(id)}, { $set: updateRequest });
 }
 export async function deleteMovieByID(id) {
-  return await client.db("Ragav").collection("movies").deleteOne({ _id: ObjectId(id)});
+  return await client.db("Ragav").collection("movies").deleteOne({ _id: new ObjectId(id)});
 }
 export async function createMovies(createRequest) {
   return await client.db("Ragav").collection("movies").insertMany(createRequest);
 }
 export async function getMovieByID(id) {
   // console.log("****", id)
-  return await client.db("Ragav").collection("movies").findOne({ _id : ObjectId(id) });
+  return await client.db("Ragav").collection("movies").findOne({ _id : new ObjectId(id) });
 }
 export async function getMovies(request) {
   return await client.db("Ragav").collection("movies").find(request.query).toArray();
